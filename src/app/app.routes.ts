cd93f961f74926e9acb9dada69ee08c1c5120785
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-09-11 11:59:53 -0300

    feat: create rotas para paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditarComponent } from './pages/editar/editar.component';
+import { DetalhesComponent } from './pages/detalhes/detalhes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'cadastro', component: CadastroComponent},
+  {path: '', component: HomeComponent},
+  {path: 'editar/:id', component: EditarComponent},
+  {path: 'detalhes/:id', component: DetalhesComponent},
+];
